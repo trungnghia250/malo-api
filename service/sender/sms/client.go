b280ed96c4e5d1a4a4314d00ae7baadec49a0310
Stacktee <stacktee@Stacktees-MacBook-Pro.local>
2023-06-02 18:50:04 +0700

    update sms config

--- service/sender/sms/client.go
+++ service/sender/sms/client.go
@@ -14,18 +14,18 @@ type Request struct {
 }
 
 func Send(request Request) ([]string, error) {
-	accountSid := "AC3875387c245514321b16003f0f109794"
-	authToken := "736de2bfadf1fe37ad6f4cfc6ea201e3"
+	accountSid := "AC74acd33e928f3b9cfdfba86132a51d2e"
+	authToken := "eefe57305498e33688a3ce0fd253377e"
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
 		Password: authToken,
 	})
 	params := &api.CreateMessageParams{}
-	params.SetFrom("+15635002841")
+	params.SetFrom("+13612822731")
 	params.SetBody(request.Content)
 	if request.SendAt > 0 {
 		params.SetSendAt(time.Unix(int64(request.SendAt), 0))
-		params.SetMessagingServiceSid("MG28c99338dbe0159c1a609d0e9f2865db")
+		params.SetMessagingServiceSid("MG4fe28942242424956b087cd4794a3333")
 		params.SetScheduleType("fixed")
 	}
 	var messageSid []string
@@ -42,8 +42,8 @@ func Send(request Request) ([]string, error) {
 }
 
 func Cancel(messageSIDs []string) error {
-	accountSid := "AC3875387c245514321b16003f0f109794"
-	authToken := "736de2bfadf1fe37ad6f4cfc6ea201e3"
+	accountSid := "AC74acd33e928f3b9cfdfba86132a51d2e"
+	authToken := "eefe57305498e33688a3ce0fd253377e"
 	client := twilio.NewRestClientWithParams(twilio.ClientParams{
 		Username: accountSid,
 		Password: authToken,
